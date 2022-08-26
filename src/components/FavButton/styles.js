f1b459d700607fa6ed324f3ee8e0af9f127940b4
emanuel <emanuel@serfe.com>
2022-08-26 15:10:23 -0300

    instagram de mascotas

---
+++ src/components/FavButton/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+export const Button = styled.button`
+  display: flex;
+  margin-left:21px;
+  color: white;
+  align-items: center;
+  padding-top: 8px;
+  & svg {
+    margin-right: 4px;
+  }
+`;
